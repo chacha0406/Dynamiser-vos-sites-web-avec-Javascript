a1f85bb9f974c7c5722b12aa70583a53e0cd5187
chacha <charlotte.heliou@gmail.com>
2021-02-22 14:56:20 +0100

    Création fonction NewGame

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 /*Déclaration des variables*/
-
+let startGame, diceNumber, playerScore, currentScore
 
 
 /*Bouton NEW GAME*/
@@ -12,6 +12,11 @@ function NewGame() {
     document.getElementById('score-temp-joueur2').textContent = 0;
     document.getElementById('résultat-dé').src = '../Jeu/Images/1.png';
 
+    startGame = true;
+    diceNumber = 0;
+    playerScore = 0;
+    currentScore = 0;
+
     alert('Vous allez commencer une nouvelle partie')
 } 
 
@@ -19,13 +24,19 @@ newgame.addEventListener('click', NewGame)
 
 /*Bouton ROLL DICE*/
 var rolldice = document.getElementById('rolldice')
+
 function RollDice() {
     alert('Vous allez lancer le dé')
-} rolldice.addEventListener('click', RollDice)
+} 
+
+rolldice.addEventListener('click', RollDice)
 
 
 /*Bouton HOLD*/
 var hold = document.getElementById('hold')
+
 function Hold() {
     alert('Vous allez sauvegarder vos points')
-} hold.addEventListener('click', Hold)
+} 
+
+hold.addEventListener('click', Hold)
